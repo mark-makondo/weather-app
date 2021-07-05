d09291c43dfa14696ea3c021507949e917fc1631
Ronie <vandyke1906@gmail.com>
2021-07-06 02:51:43 +0800

    add cron scheduler

---
+++ backend/src/config/scheduler.js
@@ -0,0 +1,34 @@
+const cron = require('node-cron');
+
+const fetch = require('node-fetch');
+
+const EmailSender = require('../services/emailSender');
+
+class Scheduler {
+  schedule() {
+    //run every 30 seconds
+    // cron.schedule('*/30 * * * * *', async () => {
+
+    //run every 30 minues
+    cron.schedule('30 * * * * *', async () => {
+      console.log('Running scheduler...');
+
+      //fetch openweather result
+      const city = 'Manila';
+      const response = await fetch(
+        `${process.env.OPEN_WEATHER_URI}/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API}`
+      );
+      const data = await response.json();
+
+      const emailSender = new EmailSender();
+      const options = {
+        sendTo: 'vandyke1906@gmail.com',
+        subject: 'Mini App Result',
+        htmlMessage: `<h1> Test Message here </h1> </br> <pre>${JSON.stringify(data, null, 2)}</pre>`,
+      };
+      emailSender.send(options);
+    });
+  }
+}
+
+module.exports = Scheduler;
