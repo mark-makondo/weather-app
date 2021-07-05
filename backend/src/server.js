6ae57f9c96e3f30b9f5b52f19427fdde2b5fbaf8
Ronie <vandyke1906@gmail.com>
2021-07-06 02:52:37 +0800

    update test server cron

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,10 @@ const puppeteerRoutes = require('./routers/puppeteerRoutes');
 const cors = require('cors');
 const express = require('express');
 
+// const cron = require('node-cron');
+// const fetch = require('node-fetch');
+const Scheduler = require('./config/scheduler');
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -14,5 +18,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/puppeteer', puppeteerRoutes);
+
+const sched = new Scheduler();
+sched.schedule();
+
+// cron.schedule('* * * * *', () => {
+//   //   console.log('running a task every minute ' + new Date().toISOString());
+//   fetch('https://api.openweathermap.org/data/2.5/weather?q=Manila&appid=eec5077e47c16806527430b6711b06a5')
+//     .then((res) => res.json())
+//     .then((json) => console.log(json));
+// });
 
 app.listen(PORT, () => console.log(`Listening to PORT: ${PORT}`));
