7124ad768aef335543ed421fb17fc60295bf1bca
Ronie <vandyke1906@gmail.com>
2021-07-07 02:13:15 +0800

    update specific methods for source and target

--- client/src/components/flowApp/FlowApp.js
+++ client/src/components/flowApp/FlowApp.js
@@ -11,67 +11,72 @@ import weatherAppImage from '../../assets/weather-app.png';
 
 import JsPlumb from '../../helper/jsplumb';
 
+// settings
+import settings from '../../settings';
+
 const appImages = {
   '60e45ea77d7c08c83a79f65e': { imageSrc: locationAppImage },
   '60e459dbf66e1733407259cb': { imageSrc: weatherAppImage },
 };
 
-// const availableMethodsData = {
-//   geolocation: { data: ['Get Location Details'], imageSrc: locationAppImage },
-//   open_weather: {
-//     data: ['Get By City', 'Get By City ID', 'Get By Coordinates'],
-//     imageSrc: weatherAppImage,
-//   },
-// };
-
 const jsPlumb = new JsPlumb('diagram');
 
 const FlowApp = ({ onOpenWeatherScrape, loading, supportedApps }) => {
-  const [methodsAvailable, setMethodsAvailable] = useState([]);
-  console.log(supportedApps);
+  const [sourceMethod, setSourceMethod] = useState([]);
+  const [targetMethod, setTargetMethod] = useState([]);
+  const [params, setParams] = useState({});
+  const [enableTargetAppSelection, setEnableTargetAppSelection] = useState(false);
 
   const [firstAppForm] = Form.useForm();
   const [secondAppForm] = Form.useForm();
 
   const handleSelectAppChange = (value, formReference) => {
-    // const getAppMethods = availableMethodsData[value];
-    // setMethodsAvailable(getAppMethods.data);
     const selectedMethod = supportedApps.filter((s) => s._id === value)[0].methods;
-    setMethodsAvailable(selectedMethod);
-    console.log(appImages[value], 'asdsa');
+
     if (formReference === firstAppForm) {
+      setSourceMethod(selectedMethod);
       document.getElementById('firstAppImage').setAttribute('src', appImages[value].imageSrc);
       //adding endpoint for jsplumb
       addJsPlumbEndPoint('endpoint1', 'Right', true);
+      setEnableTargetAppSelection(true);
     } else if (formReference === secondAppForm) {
+      setTargetMethod(selectedMethod);
       document.getElementById('secondAppImage').setAttribute('src', appImages[value].imageSrc);
       //adding endpoint for jsplumb
       addJsPlumbEndPoint('endpoint2', 'Left');
-      jsPlumb.get('jsPlumbInstance').connect({
-        source: 'endpoint1',
-        target: 'endpoint2',
-        anchors: ['Right', 'Left'],
-        endpoint: ['Dot', { radius: 20 }],
-        endpointStyle: { fill: 'gray' },
-        paintStyle: { stroke: 'gray', strokeWidth: 10, dashstyle: '5 2' },
-        hoverPaintStyle: { stroke: '#f44336', strokeWidth: 10 },
-      });
-    }
 
-    // const imgElement =
-    //   formReference === firstAppForm
-    //     ? document.getElementById('firstAppImage')
-    //     : document.getElementById('secondAppImage');
-    // imgElement.setAttribute('src', getAppMethods.imageSrc);
+      jsPlumb.connectEndPoints('endpoint1', 'endpoint2');
+    }
+  };
+
+  const handleSelectMethod = (value, formReference) => {
+    const methods = formReference === firstAppForm ? sourceMethod : targetMethod;
+    const selected = methods[value];
+    const apiUri = selected.api[0];
+    const baseUri = `http://${apiUri.split('?')[0]}?`;
+    for (const p of selected.required) {
+      if (p[0] !== 'appid') console.log(p[0], p[2]);
+      else
+        setParams((oldState) => {
+          return {
+            ...oldState,
+            appid: settings.weather.API_KEY,
+          };
+        });
+    }
   };
 
   const endPointContentOptions = (formReference) => {
+    let methods = formReference === firstAppForm ? sourceMethod : targetMethod;
     return (
       <div className="endpoint_container" style={{ minWidth: '250px', width: '250px' }}>
         <Form form={formReference} layout="vertical">
           <Space direction="vertical" style={{ width: 'calc(100%)' }}>
             <Form.Item label="Select App" name="appSelected">
-              <Select onChange={(value) => handleSelectAppChange(value, formReference)}>
+              <Select
+                onChange={(value) => handleSelectAppChange(value, formReference)}
+                disabled={formReference === secondAppForm && !enableTargetAppSelection}
+              >
                 {supportedApps.map(({ _id, name }) => (
                   <Select.Option key={_id}>{name}</Select.Option>
                 ))}
@@ -79,8 +84,8 @@ const FlowApp = ({ onOpenWeatherScrape, loading, supportedApps }) => {
             </Form.Item>
 
             <Form.Item label="Select Method" name="methodSelected">
-              <Select>
-                {methodsAvailable.map((value, i) => (
+              <Select onChange={(value) => handleSelectMethod(value, formReference)}>
+                {methods.map((value, i) => (
                   <Select.Option key={i}>{value.title}</Select.Option>
                 ))}
               </Select>
@@ -107,24 +112,24 @@ const FlowApp = ({ onOpenWeatherScrape, loading, supportedApps }) => {
     if (currentAppSelected !== undefined) {
       // const getAppMethods = availableMethodsData[currentAppSelected];
       // setMethodsAvailable(getAppMethods.data);
-
-      const selectedMethod = supportedApps.filter((s) => s._id === currentAppSelected)[0].methods;
-      setMethodsAvailable(selectedMethod);
+      //   const selectedMethod = supportedApps.filter((s) => s._id === currentAppSelected)[0].methods;
+      //   setMethodsAvailable(selectedMethod);
     }
   };
 
+  const handleSchedule = () => {};
+
   const addJsPlumbEndPoint = (elementId, anchor, isSource = false, parameters = {}) => {
     jsPlumb.addEndPoint(elementId, {
-      endpoint: 'Dot',
-      //   anchor: 'BottomCenter',
-      //   isTarget: true,
-      //   isSource: true,
       anchor: anchor,
-      detachable: false,
-      isSource: isSource,
-      isTarget: !isSource,
-      parameters: parameters,
-      connectionType: 'connector-style',
+      //   endpoint: 'Dot',
+      //   //   anchor: 'BottomCenter',
+      //   //   isTarget: true,
+      //   //   isSource: true,
+      //   isSource: isSource,
+      //   isTarget: !isSource,
+      //   parameters: parameters,
+      //   connectionType: 'connector-style',
     });
     //set draggable
     jsPlumb.setElementDraggable(elementId);
@@ -132,13 +137,19 @@ const FlowApp = ({ onOpenWeatherScrape, loading, supportedApps }) => {
 
   return (
     <div className="flow_app">
-      <Button type="primary" onClick={getDetails}>
-        Get App Details
-      </Button>
-
-      <Button onClick={onOpenWeatherScrape} loading={loading}>
-        Scrape Weather API
-      </Button>
+      <div>
+        <Space align={'center'} style={{ width: 'calc(100%)' }}>
+          <Button type="dashed" onClick={handleSchedule} danger>
+            Schedule Settings
+          </Button>
+          <Button type="primary" onClick={getDetails}>
+            Save Automation
+          </Button>
+          <Button onClick={onOpenWeatherScrape} loading={loading}>
+            Scrape Weather API
+          </Button>
+        </Space>
+      </div>
       <div id="diagram">
         <div id="endpoint1" className="control">
           <Popover
